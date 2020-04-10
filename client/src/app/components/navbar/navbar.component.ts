8a8230d7eb187b46738f6917dee9dc4b9dab9af1
Vladimir Bozhkov <vladibo@pop-os.localdomain>
2020-04-10 22:54:19 +0300

    added logout

--- client/src/app/components/navbar/navbar.component.ts
+++ client/src/app/components/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { AuthService } from 'src/app/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
 	selector: 'app-navbar',
@@ -14,14 +15,21 @@ export class NavbarComponent implements OnInit, OnDestroy {
 
 	private authListenerSubs: Subscription;
 
-	constructor(private authService: AuthService) {}
+	constructor(private authService: AuthService, private router: Router) {}
+
+	logout() {
+		console.log('loggin out');
+		localStorage.clear();
+		this.isAuth = this.authService.isLoggedIn();
+		this.router.navigate([ '/login' ]);
+	}
 
 	ngOnInit(): void {
 		// this.isAuth = this.authService.getIsAuth();
 		// this.authListenerSubs = this.authService
 		// 	.getAuthStatusListener()
 		// 	.subscribe((isAuth) => (this.userIsAuth = isAuth));
-		this.isAuth = localStorage.getItem('token');
+		this.isAuth = this.authService.isLoggedIn();
 	}
 
 	ngOnDestroy(): void {
